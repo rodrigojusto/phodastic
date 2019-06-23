c815c43a3f32f3f7ef8d9da1dcc6f650bbf3c119
Jorge Gonçalves Junior <jorge.techinfo@gmail.com>
2019-06-23 19:53:25 -0300

    Feature harmonic (#2)
    * harmonicMean refactory
    
    cleans some logic, applies native php array functions and throws an exception without return any value
    
    * rename function
    
    * change harmonicMain function

--- src/Math/Math.php
+++ src/Math/Math.php
@@ -8,52 +8,54 @@
 
 namespace RodrigoJusto\Phodastic\Math;
 
-use \Exception;
+use Exception;
 
 class Math
 {
-    public static function harmonicMeanIgnoreInvalidValues(array $values, int $countValues=null ): float
+    /**
+     * Get the HarmonicMean
+     * 
+     * @param array $values
+     * @param null $count
+     * @param bool $ignoreInvalidValues
+     * @return float     
+     *
+     */ 
+    public static function harmonicMean(array $values, $count = null, bool $ignoreInvalidValues = false)
     {
-        /** Initializes $sum with 0 */
-        $sum = 0.0;
-        /** Verifies if countValues has especified otherwise get from size of $values */
-        if($countValues == null){
-            $countValues = sizeof($values);
+        if(is_numeric($count) && $count != null){
+            $values = array_splice($values, 0, $count);
         }
-        foreach ($values as $value){
-            /** Checks if it is a valid number otherwise subtract 1 from $ countValue and ignore the current value */
-            if(!is_numeric($value) || $value==0){
-                $countValues--;
-                continue;
+
+        // Applies function param shift
+        if(func_num_args() == 2 && is_bool($count)) {
+            $ignoreInvalidValues = $count;
+        }
+
+        $values = array_map(function($value) use ($ignoreInvalidValues){
+            if ($ignoreInvalidValues && !is_numeric($value)) {
+                return null;
             }
 
-            $sum = $sum + (float)(1/$value);
-        }
-        /** Calculate Harmonic Mean and return after */
-        $hmean = (float)($countValues/$sum);
-
-        unset($sum, $values, $countValues);
-        return $hmean;
-    }
-
-    public static function harmonicMean(array $values, int $countValues=null ): float
-    {
-        /** Initializes $sum with 0 */
-        $sum = 0.0;
-        /** Verifies if countValues has especified otherwise get from size of $values */
-        if($countValues == null){
-            $countValues = sizeof($values);
-        }
-        foreach ($values as $value){
-            try{
-                $sum = $sum + (float)(1/$value);
-            }catch (Exception $e){
-                return $e->getMessage();
+            if(!is_numeric($value)) {
+                throw new Exception('Null, Zero or non numeric values found in array.');
             }
-        }
-        /** Calculate Harmonic Mean and return after */
-        $hmean = (float)($countValues/$sum);
-        return $hmean;
-    }
 
+            return $value + 0;
+
+        }, $values);
+
+        if($ignoreInvalidValues) {
+            $values = array_filter($values, function ($value) {
+                return $value !== null;
+            });
+        }
+
+        $mean = array_reduce($values, function($carry, $value) use ($ignoreInvalidValues) {
+            return $carry + (float) 1 / $value;
+        }, 0);
+
+        return (float) sizeof($values) / $mean;
+
+    }
 }
