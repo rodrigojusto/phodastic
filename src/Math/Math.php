f06f44156a0caf71a3061dab6ecea729691f4d2d
zeusjammer <rodrigo.justo@outlook.com.br>
2019-06-23 19:44:45 -0300

    Created Finantial class

--- src/Math/Math.php
+++ src/Math/Math.php
@@ -56,4 +56,16 @@ class Math
         $hmean = (float)($countValues/$sum);
         return $hmean;
     }
+
+    /** Finantial Math interests (need to be refactor) **/
+    public static function simpleInterest(float $capital, float $tax, int $period):float{
+        $interest = $capital * $tax * $time;
+        return $interest;
+    }
+
+    public static function compoundInterest(float $capital,float $tax, int $period):float{
+        $interest = $capital * (1+$tax) ^ $period;
+        return $interest;
+    }
+
 }
